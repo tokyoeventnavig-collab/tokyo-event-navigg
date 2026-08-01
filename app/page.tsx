d05d18cbfd982c11505b1594a069d8c58d3b3d06
tokyoeventnavig-collab <y75405332@gmail.com>
2026-08-02 05:26:51 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { getEvents } from "../lib/notion";
 
 export const revalidate = 300;
@@ -11,7 +12,11 @@ export default async function Home() {
         <div className="container">
           <p className="eyebrow">TOKYO EVENT NAVI</p>
           <h1>東京イベントナビ</h1>
-          <p className="lead">東京で開催される飲み会・交流会・趣味イベントを掲載しています。</p>
+
+          <p className="lead">
+            東京で開催される飲み会・交流会・趣味イベントを
+            掲載しています。
+          </p>
         </div>
       </header>
 
@@ -23,44 +28,75 @@ export default async function Home() {
 
         {events.length === 0 ? (
           <div className="empty">
-            公開対象のイベントがありません。Notionの「公開」をオンにしてください。
+            現在、公開中のイベントはありません。
           </div>
         ) : (
           <div className="grid">
             {events.map((event) => (
               <article className="card" key={event.id}>
-                {event.image ? (
-                  // eslint-disable-next-line @next/next/no-img-element
-                  <img src={event.image} alt="" className="image" />
-                ) : (
-                  <div className="image placeholder">TOKYO EVENT NAVI</div>
-                )}
+                <Link href={`/events/${event.id}`}>
+                  {event.image ? (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                      src={event.image}
+                      alt={event.title}
+                      className="image"
+                    />
+                  ) : (
+                    <div className="image placeholder">
+                      TOKYO EVENT NAVI
+                    </div>
+                  )}
+                </Link>
+
                 <div className="cardBody">
-                  {event.category && <div className="category">{event.category}</div>}
-                  <h3>{event.title}</h3>
+                  {event.category && (
+                    <div className="category">
+                      {event.category}
+                    </div>
+                  )}
+
+                  <h3>
+                    <Link
+                      href={`/events/${event.id}`}
+                      style={{
+                        color: "inherit",
+                        textDecoration: "none",
+                      }}
+                    >
+                      {event.title}
+                    </Link>
+                  </h3>
+
                   <dl>
                     {event.date && (
                       <>
-                        <dt>日時</dt><dd>{event.date}</dd>
+                        <dt>日時</dt>
+                        <dd>{event.date}</dd>
                       </>
                     )}
+
                     {event.location && (
                       <>
-                        <dt>場所</dt><dd>{event.location}</dd>
+                        <dt>場所</dt>
+                        <dd>{event.location}</dd>
                       </>
                     )}
+
                     {event.price && (
                       <>
-                        <dt>料金</dt><dd>{event.price}</dd>
+                        <dt>料金</dt>
+                        <dd>{event.price}</dd>
                       </>
                     )}
                   </dl>
-                  {event.description && <p className="description">{event.description}</p>}
-                  {event.url && (
-                    <a className="button" href={event.url} target="_blank" rel="noreferrer">
-                      詳細・申し込み
-                    </a>
-                  )}
+
+                  <Link
+                    className="button"
+                    href={`/events/${event.id}`}
+                  >
+                    詳細を見る
+                  </Link>
                 </div>
               </article>
             ))}
